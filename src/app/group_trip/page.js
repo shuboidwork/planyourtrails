5916733015404c6c29001baab3e8bd48109f7328
Shubham Chintalwar <shubham.c@gigindia.in>
2024-01-21 00:44:40 +0530

    Updated group trip page

--- src/app/group_trip/page.js
+++ src/app/group_trip/page.js
@@ -4,31 +4,116 @@ import LeadGen, {
 import Footer from "../../../components/common_components/footer";
 import { DestinationIntro } from "../../../components/landing_components/intro";
 
-const description =
-  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
-
-const activities = [
+const itinerary = [
   {
     id: 1,
-    dest: "Tanah Lot Temple",
-    media_url: "/common/travel1.jpg",
+    description: "It is a long established ",
+    activities: [
+      {
+        id: 1,
+        dest: "Tanah Lot Temple",
+        media_url: "/common/travel1.jpg",
+      },
+      {
+        id: 2,
+        dest: "Nusa Penida",
+        media_url: "/common/travel2.jpg",
+      },
+      {
+        id: 3,
+        dest: "Paragliding",
+        media_url: "/common/travel3.jpg",
+      },
+      {
+        id: 4,
+        dest: "Oasis Restaurant",
+        media_url: "/destinations/thailand.jpg",
+      },
+    ],
   },
   {
     id: 2,
-    dest: "Nusa Penida",
-    media_url: "/common/travel2.jpg",
+    description:
+      "jhfy accident, sometimes on purpose (injected humour and the like).",
+    activities: [
+      {
+        id: 1,
+        dest: "Tanah Lot Temple",
+        media_url: "/common/travel1.jpg",
+      },
+      {
+        id: 2,
+        dest: "Nusa Penida",
+        media_url: "/common/travel2.jpg",
+      },
+      {
+        id: 3,
+        dest: "Paragliding",
+        media_url: "/common/travel3.jpg",
+      },
+      {
+        id: 4,
+        dest: "Oasis Restaurant",
+        media_url: "/destinations/thailand.jpg",
+      },
+    ],
   },
   {
     id: 3,
-    dest: "Paragliding",
-    media_url: "/common/travel3.jpg",
+    description:
+      "It is a long he  it look liw use Loreuncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    activities: [
+      {
+        id: 1,
+        dest: "Tanah Lot Temple",
+        media_url: "/common/travel1.jpg",
+      },
+      {
+        id: 2,
+        dest: "Nusa Penida",
+        media_url: "/common/travel2.jpg",
+      },
+      {
+        id: 3,
+        dest: "Paragliding",
+        media_url: "/common/travel3.jpg",
+      },
+      {
+        id: 4,
+        dest: "Oasis Restaurant",
+        media_url: "/destinations/thailand.jpg",
+      },
+    ],
   },
   {
     id: 4,
-    dest: "Oasis Restaurant",
-    media_url: "/destinations/thailand.jpg",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    activities: [
+      {
+        id: 1,
+        dest: "Tanah Lot Temple",
+        media_url: "/common/travel1.jpg",
+      },
+      {
+        id: 2,
+        dest: "Nusa Penida",
+        media_url: "/common/travel2.jpg",
+      },
+      {
+        id: 3,
+        dest: "Paragliding",
+        media_url: "/common/travel3.jpg",
+      },
+      {
+        id: 4,
+        dest: "Oasis Restaurant",
+        media_url: "/destinations/thailand.jpg",
+      },
+    ],
   },
 ];
+
 //
 
 export default function destination() {
@@ -47,64 +132,84 @@ export default function destination() {
         </div>
       </div>
       <div className="lg:px-20 md:px-20 px-5">
-        {activities.map((activity) => (
+        {itinerary.map((day) => (
           <div
-            key={activity.id}
-            className=" mt-10 mx-auto  rounded-lg bg-white shadow-2xl border border-gray-600"
+            key={day.id}
+            className=" mt-10 mx-auto  rounded-lg bg-[url('/common/paperbackedge.png')] shadow-2xl border border-gray-600"
           >
             <div className="h-10 bg-black text-white text-2xl pl-2 pb-2 bold align-center rounded-t-lg">
               {" "}
-              Day {activity.id}
+              Day {day.id}
             </div>
             <div className="flex rounded-2xl sm:justify-stretch md:justify-center lg:justify-center items-center overflow-auto no-scrollbar  grid-rows-1 grid-flow-col scroll-pl-6 snap-x">
-              <div className="snap-center">
-                <div
-                  style={{ "--image-url": `url(${activity.media_url})` }}
-                  className={` bg-cover bg-opacity-50 bg-[image:var(--image-url)] w-40 h-40 rounded-bl-lg col-span-2 m-3`}
-                ></div>
-                <div className=" text-sm  bold font-denton text-center">
-                  {activity.dest}
+              {day.activities.map((activity) => (
+                <div key={activity.id} className="snap-center">
+                  <div
+                    style={{ "--image-url": `url(${activity.media_url})` }}
+                    className={` bg-cover bg-opacity-50 bg-[image:var(--image-url)] w-40 h-40 rounded-bl-lg col-span-2 m-3`}
+                  ></div>
+                  <div className=" text-sm  bold font-denton text-center">
+                    {activity.dest}
+                  </div>
                 </div>
-              </div>
-              <div className="snap-center">
-                <div
-                  style={{ "--image-url": `url(${activity.media_url})` }}
-                  className={` bg-cover bg-opacity-50 bg-[image:var(--image-url)] w-40 h-40 rounded-bl-lg col-span-2 m-3`}
-                ></div>
-                <div className=" text-sm  bold font-denton text-center">
-                  {activity.dest}
-                </div>
-              </div>
-              <div className="snap-center">
-                <div
-                  style={{ "--image-url": `url(${activity.media_url})` }}
-                  className={` bg-cover bg-opacity-50 bg-[image:var(--image-url)] w-40 h-40 rounded-bl-lg col-span-2 m-3`}
-                ></div>
-                <div className=" text-sm  bold font-denton text-center">
-                  {activity.dest}
-                </div>
-              </div>
-              <div className="snap-center">
-                <div
-                  style={{ "--image-url": `url(${activity.media_url})` }}
-                  className={` bg-cover bg-opacity-50 bg-[image:var(--image-url)] w-40 h-40 rounded-bl-lg col-span-2 m-3`}
-                ></div>
-                <div className=" text-sm  bold font-denton text-center">
-                  {activity.dest}
-                </div>
-              </div>
+              ))}
             </div>
 
-            <div className=" col-span-1 border border-black bg-black">
-              <p className="text-white bold text-center md:text-lg lg:text-lg sm:text-sm font-roboto p-2">
-                This is the description regarding the store owner and their
-                journey so far in travel space. This will include how many
-                countries they have travelled and how many destinations they are
-                expert in.
+            <div className=" col-span-1 border border-black bg-gray-900">
+              <p className="text-white bold md:text-lg lg:text-lg sm:text-sm p-2">
+                {day.description}
               </p>
+              <ul class="list-none text-white p-2">
+                <li>
+                  Now this is a story all about how, my life got flipped-turned
+                  upside down
+                </li>
+                <li>
+                  Now this is a story all about how, my life got flipped-turned
+                  upside down
+                </li>
+              </ul>
             </div>
           </div>
         ))}
+        <div className=" mt-10 mx-auto  rounded-lg bg-white shadow-2xl border border-gray-600">
+          <div className="h-10 bg-black text-white text-2xl pl-2 pb-2 bold align-center rounded-t-lg">
+            {" "}
+            Inclusions
+          </div>
+
+          <div className=" col-span-1 border border-black bg-gray-900 p-2">
+            <ul class="list-disc text-white p-2">
+              <li>
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li>
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className=" mt-10 mx-auto  rounded-lg bg-white shadow-2xl border border-gray-600">
+          <div className="h-10 bg-black text-white text-2xl pl-2 pb-2 bold align-center rounded-t-lg">
+            {" "}
+            Exclusions
+          </div>
+
+          <div className=" col-span-1 border border-black bg-gray-900 p-2">
+            <ul class="list-disc text-white p-2">
+              <li>
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+              <li>
+                Now this is a story all about how, my life got flipped-turned
+                upside down
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div className="">
