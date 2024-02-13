b07bf13aba66df3572f0d649f5e9fb9062418038
Shubham Chintalwar <shubham.c@gigindia.in>
2024-02-14 00:20:00 +0530

    Modal updated

--- components/landing_components/leadgenform.jsx
+++ components/landing_components/leadgenform.jsx
@@ -28,7 +28,7 @@ const SuccessModal = ({ onClose }) => {
             {/* Using useRouter to dismiss modal*/}
             <button
               onClick={onClose}
-              className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+              className="px-4 py-2 bg-obtext text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
             >
               Close
             </button>
