c5138d0207a68fcc7a6d43f006177454c27cec21
Shubham Chintalwar <shubham.c@gigindia.in>
2024-06-27 18:58:49 +0530

    Updated placeholder

--- components/landing_components/leadgenform.jsx
+++ components/landing_components/leadgenform.jsx
@@ -324,6 +324,7 @@ export default function LeadGenForm() {
                   name="number_of_days"
                   id="number_of_days"
                   max="30"
+                  placeholder="5"
                   required="true"
                   className="block w-full rounded-md border-0 px-3.5 py-2  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-obtext sm:text-sm sm:leading-6"
                 />
